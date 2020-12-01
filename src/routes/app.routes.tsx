4910d3b35d8b4ea799e78bcc0944960f64d35192
erick <erick.piazza@brainweb.com.br>
2020-12-01 09:22:30 -0300

    ajustes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
 
-import Dashboard from '../pages/Dashboard/index';
+import ProvidersList from '../pages/ProvidersList/index';
+import {useAuth} from '../../src/hooks/auth';
 
-const App = createStackNavigator();
+const App = createBottomTabNavigator();
+const RoutersHome = createStackNavigator();
+
+function ScreensHome() {
+  const {user} = useAuth();
+  return (
+    <RoutersHome.Navigator>
+      <RoutersHome.Screen
+        options={{title: `Bem vindo ${user.name} `}}
+        name="Search"
+        component={ProvidersList}
+      />
+    </RoutersHome.Navigator>
+  );
+}
 
 const AppRoutes: React.FC = () => (
   <App.Navigator
-    screenOptions={{
-      headerShown: false,
-      cardStyle: {backgroundColor: '#fff'},
+    tabBarOptions={{
+      activeTintColor: '#0099FF',
+      showLabel: false,
+      tabStyle: {borderColor: 'rgba(0, 153, 255, 0.308)', borderTopWidth: 1},
     }}>
-    <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen
+      name="dasd "
+      component={ScreensHome}
+      options={{
+        tabBarIcon: ({color}) => <Icon name="home" color={color} size={34} />,
+      }}
+    />
   </App.Navigator>
 );
 
